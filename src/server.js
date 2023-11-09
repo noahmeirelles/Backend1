8e283c6a671c5449193d888d936d7ef91c51cf5a
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-11-09 17:03:05 -0300

    feat: done

--- src/server.js
+++ src/server.js
@@ -8,10 +8,29 @@ const port = process.env.PORT || 5000
 const noah = express();//cria o servidor
 noah.use(express.json());
 
-noah.get("/", (req, res)=>
-res.status(200).json({message: "Hello, World!"})
+noah.get("/:nome", (req, res)=>{
+const nome = req.params.nome;
+const pratos = req.query.pratos;
+const maracutaia = req.body.maracutaia
+
+res.status(200).json({firstmessage : `hola${nome}` ,last:`${pratos}`,maracutaia : maracutaia })}
 )
 
-noah.listen(port, ()=>
-console.log(`Server startedn on http://localhost:${port}`)
+    
+
+noah.listen(port, ()=>{
+console.log(`Server startedn on http://localhost:${port}`)}
+)
+
+noah.post("/", (req, res)=>{
+    const nome = req.query.nome;
+res.status(200).json({message: `Hello, POST! ${nome}`})}
+)
+noah.put("/", (req, res)=>{
+    const nome = req.query.nome;
+res.status(200).json({message: `Hello, PUT! ${nome}`})}
+)
+noah.delete("/", (req, res)=>{
+    const nome = req.query.nome;
+res.status(200).json({message: `Hello, DELETE! ${nome}`})}
 )
